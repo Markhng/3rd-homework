c14fd09781b8f3807d29a30d5037658165699bba
Markhng <32028289+Markhng@users.noreply.github.com>
2017-11-29 22:58:50 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -2,63 +2,63 @@
 #include <stdlib.h>
 #include <time.h>
 
-int ran(void)
-{
-    int r;
-    srand(time(0));
-    r = rand() % 52;
-    return r;
-}
+#define TURNS 20
 
-void printb(void)
-{
+//#include<stdarg.h>
 
-}
-int main()
+struct body                     //定义玩家属性
+{
+    int hp[TURNS];     //血量
+    int atk[TURNS];    //攻击力
+    int def[TURNS];    //防御力
+    int crt[TURNS];    //暴击率
+    int evd[TURNS];    //闪避率   不知道攻速是做什么用的就没定义
+}p[2];
+int t = 0 ;
+
+int ran(int, int);      //整数区间随机函数
+void body_show(int);            //显示玩家属性
+void atk(void);
+
+int main(void)
 {
     srand(time(0));
-    struct
-    {
-        unsigned char col;
-        unsigned int num;
-    } b[52];
-    for(int i1 = 0 ; i1 < 4 ; i1 ++)
-    {
-        for(int i2 = 0 ; i2 < 13 ; i2 ++)
-        {
-            switch (i1)
-            {
-            case 0 :
-                b[i1*13 + i2].col = 'S';
-                break;
-            case 1 :
-                b[i1*13 + i2].col = 'H';
-                break;
-            case 2 :
-                b[i1*13 + i2].col = 'D';
-                break;
-            case 3 :
-                b[i1*13 + i2].col = 'C';
-                break;
-            }
-            b[i1*13 + i2].num = i2;
-        }
-    }
-    printf("P1\tP2\tP3\tP4\n\n");
-    for(int i3 = 0 ; i3 < 13 ; i3 ++)
-    {
-        for (int i,i4 = 0 ; i4 < 4 ; i4 ++)
-        {
-            do
-            {
-                i = rand() % 52;
-            }
-            while(b[i].col == '\0');
-            printf("%c %d\t",b[i].col,b[i].num + 1);
-            b[i].col = '\0';
-        }
-        printf("\n");
-    }
-    system("pause");
+    for(;! p[0],hp[t] && ! p[1],hp[t] ; t ++)
+    body_show(0);
     return 0;
 }
+
+int ran(int ran_var_0, int ran_var_1)
+{
+    int ran_rst;
+    ran_rst = rand() % (ran_var_1 - ran_var_0 + 1) + ran_var_0;
+    return ran_rst;
+}
+
+void body_show(int pi)
+{
+    printf("Player: %d\t",pi);
+    printf("Current HP: %d\t",p[pi].hp[t]);
+    printf("Lose HP: %d\t",p[pi].hp[t] - p[pi].hp[t-1]);
+}
+
+/**
+int ran(int ran_var_0, ...)
+{
+    int ran_var_1;
+    int ran_rst;
+    int ran_num;
+    int ran_chr[10];
+    va_list ran_vars;
+    va_start(ran_vars, ran_var_0);
+        ran_num = _vsnprintf(ran_chr, 10, &ran_var_0, ran_vars);
+        ran_var_1 = va_arg(ran_vars, int);
+    va_end(ran_vars);
+    printf("\n%d\n%d",ran_num,ran_var_1);
+        if (ran_num == 1)
+            ran_rst = rand() % ran_var_0;
+        else
+            ran_rst = rand() % (ran_var_1 - ran_var_0) + ran_var_0;
+    return ran_rst;
+}
+**/
